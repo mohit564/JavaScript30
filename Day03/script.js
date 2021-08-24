f2ed755c6e24700cb5c230571f1f521b5dee4e20
mohit564 <86644125+mohit564@users.noreply.github.com>
2021-08-24 14:49:06 +0530

    Day 3 completed

---
+++ Day03/script.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleChange() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("change", handleChange);
+  input.addEventListener("mouseover", handleChange);
+});
